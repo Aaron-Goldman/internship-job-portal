4c28e541dcca68807b30c7d632bffd39cc725098
Aaron-Goldman <aaron.goldman@moduscreate.com>
2021-03-17 15:33:54 -0400

    fix indentation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,21 +1,22 @@
 import { gql } from 'graphql-tag';
 
 const REGISTER = gql`
-mutation REGISTER(
-  $username: String!
-  $firstName: String!
-  $lastName: String!
-  $password: String!
-) {
-  createUser(
-    username: $username
-    firstName: $firstName
-    lastName: $lastName
-    password: $password
-    userRoleId: 3
+  mutation REGISTER(
+    $username: String!
+    $firstName: String!
+    $lastName: String!
+    $password: String!
   ) {
-    id
+    createUser(
+      username: $username
+      firstName: $firstName
+      lastName: $lastName
+      password: $password
+      userRoleId: 3
+    ) {
+      id
+    }
   }
-}`;
+`;
 
 export default REGISTER;
