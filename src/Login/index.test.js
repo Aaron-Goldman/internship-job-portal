4a03ee46dbd11230ba44cf6ffd17017ecebf04a2
Aaron-Goldman <aaron.goldman@moduscreate.com>
2021-03-17 21:33:09 -0400

    split test cases

--- src/Login/index.test.js
+++ src/Login/index.test.js
@@ -8,68 +8,106 @@ import Login from './index';
 import QUERY_USERS from '../graphql/queries';
 import { HOME_PATH, LOGIN_PATH } from '../paths';
 
-const mockedUsersResponse = {
-  request: { query: QUERY_USERS },
-  result: {
-    data: {
-      users: [
-        {
-          username: 'User',
-          password: 'password',
-          id: 1,
-        },
-      ],
+const mocks = [
+  {
+    request: { query: QUERY_USERS },
+    result: {
+      data: {
+        users: [
+          {
+            username: 'User',
+            password: 'password',
+            id: 1,
+          },
+        ],
+      },
     },
   },
-};
-
-const mocks = [
-  mockedUsersResponse,
-  mockedUsersResponse,
-  mockedUsersResponse,
 ];
 
-it('renders the login page', async () => {
-  render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <AuthProvider>
-        <Router initialEntries={[LOGIN_PATH]}>
-          <Switch>
-            <Route
-              exact
-              path={HOME_PATH}
-              component={() => <div>Home</div>}
-            />
-            <Route path={LOGIN_PATH} component={Login} />
-          </Switch>
-        </Router>
-      </AuthProvider>
-    </MockedProvider>,
-  );
-  expect(screen.getByRole('form')).toBeInTheDocument();
-  expect(screen.getByText('Register Now')).toBeInTheDocument();
-  const [loginButton] = screen.getAllByRole('button');
-  expect(loginButton).toBeDisabled();
+describe('Login page', () => {
+  const setup = () => {
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <AuthProvider>
+          <Router initialEntries={[LOGIN_PATH]}>
+            <Switch>
+              <Route
+                exact
+                path={HOME_PATH}
+                component={() => <div>Home</div>}
+              />
+              <Route path={LOGIN_PATH} component={Login} />
+            </Switch>
+          </Router>
+        </AuthProvider>
+      </MockedProvider>,
+    );
+    const [loginButton] = screen.getAllByRole('button');
+    const usernameField = screen.getByLabelText('Username');
+    const passwordField = screen.getByLabelText('Password');
+    return { loginButton, usernameField, passwordField };
+  };
 
-  const usernameField = screen.getByLabelText('Username');
-  userEvent.type(usernameField, ' {backspace}');
-  expect(await screen.findByText('Username required.')).toBeInTheDocument();
-  userEvent.type(usernameField, 'User_');
-  expect(screen.getByDisplayValue('User_')).toBeInTheDocument();
+  it('renders the login page', () => {
+    const { loginButton, usernameField, passwordField } = setup();
+    expect(screen.getByRole('form')).toBeInTheDocument();
+    expect(screen.getByText('Register Now')).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+    expect(usernameField).toBeInTheDocument();
+    expect(passwordField).toBeInTheDocument();
+    expect(loginButton).toBeDisabled();
+  });
 
-  const passwordField = screen.getByLabelText('Password');
-  userEvent.type(passwordField, ' {backspace}');
-  expect(await screen.findByText('Password required.')).toBeInTheDocument();
-  userEvent.type(passwordField, 'password');
-  expect(screen.getByDisplayValue('password')).toBeInTheDocument();
+  it('updates fields with user input', () => {
+    const { usernameField, passwordField } = setup();
+    userEvent.type(usernameField, 'User');
+    userEvent.type(passwordField, 'password');
+    expect(screen.getByDisplayValue('User')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('password')).toBeInTheDocument();
+  });
 
-  expect(loginButton).toBeEnabled();
+  it('displays an error when a field is empty', async () => {
+    const { usernameField, passwordField } = setup();
+    userEvent.type(usernameField, ' {backspace}');
+    userEvent.type(passwordField, ' {backspace}');
+    expect(await screen.findByText('Username required.')).toBeInTheDocument();
+    expect(await screen.findByText('Password required.')).toBeInTheDocument();
+  });
 
-  userEvent.click(loginButton);
-  expect(await screen.findByText('Incorrect username or password.')).toBeInTheDocument();
-  expect(localStorage.getItem('user')).toBeNull();
-  userEvent.type(usernameField, '{backspace}');
-  userEvent.click(loginButton);
-  expect(await screen.findByText('Home')).toBeInTheDocument();
-  expect(localStorage.getItem('user')).toEqual('1');
+  it('enables the login button when fields are filled', () => {
+    const { usernameField, passwordField, loginButton } = setup();
+    userEvent.type(usernameField, 'User');
+    userEvent.type(passwordField, 'password');
+    expect(loginButton).toBeEnabled();
+  });
+
+  it('displays an error for incorrect credentials', async () => {
+    const { usernameField, passwordField, loginButton } = setup();
+    userEvent.type(usernameField, 'wrong username');
+    userEvent.type(passwordField, 'wrong password');
+    userEvent.click(loginButton);
+    expect(await screen.findByText('Incorrect username or password.')).toBeInTheDocument();
+  });
+
+  it('saves user state in local storage on failed login', () => {
+    setup();
+    expect(localStorage.getItem('user')).toBeNull();
+  });
+
+  it('redirects to home path on successful login', async () => {
+    const { usernameField, passwordField, loginButton } = setup();
+    userEvent.type(usernameField, 'User');
+    userEvent.type(passwordField, 'password');
+    userEvent.click(loginButton);
+    expect(await screen.findByText('Home')).toBeInTheDocument();
+  });
+
+  it('saves user state in local storage on successful login', () => {
+    const { usernameField, passwordField, loginButton } = setup();
+    userEvent.type(usernameField, 'User');
+    userEvent.type(passwordField, 'password');
+    userEvent.click(loginButton);
+    expect(localStorage.getItem('user')).toEqual('1');
+  });
 });
--- src/Register/index.test.js
+++ src/Register/index.test.js
@@ -9,92 +9,128 @@ import QUERY_USERS from '../graphql/queries';
 import REGISTER from '../graphql/mutations';
 import { HOME_PATH, REGISTER_PATH } from '../paths';
 
-const mockedUsersResponse = {
-  request: { query: QUERY_USERS },
-  result: {
-    data: {
-      users: [
-        {
-          username: 'User',
-          password: 'password',
-          id: 1,
-        },
-      ],
-    },
-  },
-};
-
-const mockedRegisterResponse = {
-  request: {
-    query: REGISTER,
-    variables: {
-      username: 'User_',
-      firstName: 'John',
-      lastName: 'Smith',
-      password: 'password',
-    },
-  },
-  result: {
-    data: {
-      id: 1,
-    },
-  },
-};
-
 const mocks = [
-  mockedUsersResponse,
-  mockedUsersResponse,
-  mockedUsersResponse,
-  mockedRegisterResponse,
+  {
+    request: { query: QUERY_USERS },
+    result: {
+      data: {
+        users: [
+          {
+            username: 'User',
+            password: 'password',
+            id: 1,
+          },
+        ],
+      },
+    },
+  },
+  {
+    request: {
+      query: REGISTER,
+      variables: {
+        username: 'User_',
+        firstName: 'John',
+        lastName: 'Smith',
+        password: 'password',
+      },
+    },
+    result: {
+      data: {
+        id: 1,
+      },
+    },
+  },
 ];
 
-it('renders the register page', async () => {
-  render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <AuthProvider>
-        <Router initialEntries={[REGISTER_PATH]}>
-          <Switch>
-            <Route
-              exact
-              path={HOME_PATH}
-              component={() => <div>Home</div>}
-            />
-            <Route path={REGISTER_PATH} component={Register} />
-          </Switch>
-        </Router>
-      </AuthProvider>
-    </MockedProvider>,
-  );
+describe('Register page', () => {
+  const setup = () => {
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <AuthProvider>
+          <Router initialEntries={[REGISTER_PATH]}>
+            <Switch>
+              <Route
+                exact
+                path={HOME_PATH}
+                component={() => <div>Home</div>}
+              />
+              <Route path={REGISTER_PATH} component={Register} />
+            </Switch>
+          </Router>
+        </AuthProvider>
+      </MockedProvider>,
+    );
+    const usernameField = screen.getByLabelText('Username');
+    const firstNameField = screen.getByLabelText('First Name');
+    const lastNameField = screen.getByLabelText('Last Name');
+    const passwordField = screen.getByLabelText('Password');
+    const registerButton = screen.getByRole('button');
+    return {
+      usernameField, firstNameField, lastNameField, passwordField, registerButton,
+    };
+  };
 
-  expect(await screen.findByRole('form')).toBeInTheDocument();
+  it('renders the register page', () => {
+    const {
+      usernameField, firstNameField, lastNameField, passwordField, registerButton,
+    } = setup();
+    expect(screen.getByRole('form')).toBeInTheDocument();
+    expect(usernameField).toBeInTheDocument();
+    expect(firstNameField).toBeInTheDocument();
+    expect(lastNameField).toBeInTheDocument();
+    expect(passwordField).toBeInTheDocument();
+    expect(registerButton).toBeInTheDocument();
+  });
 
-  const usernameField = screen.getByLabelText('Username');
-  userEvent.type(usernameField, ' {backspace}');
-  expect(await screen.findByText('Username required.')).toBeInTheDocument();
-  userEvent.type(usernameField, 'User');
-  expect(screen.getByDisplayValue('User')).toBeInTheDocument();
+  it('updates fields with user input', () => {
+    const {
+      usernameField, firstNameField, lastNameField, passwordField,
+    } = setup();
+    userEvent.type(usernameField, 'User');
+    userEvent.type(firstNameField, 'John');
+    userEvent.type(lastNameField, 'Smith');
+    userEvent.type(passwordField, 'password');
+    expect(screen.getByDisplayValue('User')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('John')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('Smith')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('password')).toBeInTheDocument();
+  });
 
-  const firstNameField = screen.getByLabelText('First Name');
-  userEvent.type(firstNameField, ' {backspace}');
-  expect(await screen.findByText('First name required.')).toBeInTheDocument();
-  userEvent.type(firstNameField, 'John');
-  expect(screen.getByDisplayValue('John')).toBeInTheDocument();
+  it('displays an error when field is empty', async () => {
+    const {
+      usernameField, firstNameField, lastNameField, passwordField,
+    } = setup();
+    userEvent.type(usernameField, ' {backspace}');
+    expect(await screen.findByText('Username required.')).toBeInTheDocument();
+    userEvent.type(firstNameField, ' {backspace}');
+    expect(await screen.findByText('First name required.')).toBeInTheDocument();
+    userEvent.type(lastNameField, ' {backspace}');
+    expect(await screen.findByText('Last name required.')).toBeInTheDocument();
+    userEvent.type(passwordField, ' {backspace}');
+    expect(await screen.findByText('Password required.')).toBeInTheDocument();
+  });
 
-  const lastNameField = screen.getByLabelText('Last Name');
-  userEvent.type(lastNameField, ' {backspace}');
-  expect(await screen.findByText('Last name required.')).toBeInTheDocument();
-  userEvent.type(lastNameField, 'Smith');
-  expect(screen.getByDisplayValue('Smith')).toBeInTheDocument();
+  it('displays an error for duplicate username', async () => {
+    const {
+      usernameField, firstNameField, lastNameField, passwordField, registerButton,
+    } = setup();
+    userEvent.type(usernameField, 'User');
+    userEvent.type(firstNameField, 'John');
+    userEvent.type(lastNameField, 'Smith');
+    userEvent.type(passwordField, 'password');
+    userEvent.click(registerButton);
+    expect(await screen.findByText('Username taken.')).toBeInTheDocument();
+  });
 
-  const passwordField = screen.getByLabelText('Password');
-  userEvent.type(passwordField, ' {backspace}');
-  expect(await screen.findByText('Password required.')).toBeInTheDocument();
-  userEvent.type(passwordField, 'password');
-  expect(screen.getByDisplayValue('password')).toBeInTheDocument();
-
-  userEvent.click(screen.getByRole('button'));
-  expect(await screen.findByText('Username taken.')).toBeInTheDocument();
-  userEvent.type(usernameField, '_');
-  userEvent.click(screen.getByRole('button'));
-  expect(await screen.findByText('Home')).toBeInTheDocument();
+  it('redirects to home path on successful registration', async () => {
+    const {
+      usernameField, firstNameField, lastNameField, passwordField, registerButton,
+    } = setup();
+    userEvent.type(usernameField, 'User_');
+    userEvent.type(firstNameField, 'John');
+    userEvent.type(lastNameField, 'Smith');
+    userEvent.type(passwordField, 'password');
+    userEvent.click(registerButton);
+    expect(await screen.findByText('Home')).toBeInTheDocument();
+  });
 });
