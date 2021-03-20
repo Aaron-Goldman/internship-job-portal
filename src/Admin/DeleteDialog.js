5fe957b56da79867ee126a57608576d468165ea5
Aaron-Goldman <aaron.goldman@moduscreate.com>
2021-03-20 14:18:56 -0400

    use callback

--- src/Admin/DeleteDialog.js
+++ src/Admin/DeleteDialog.js
@@ -8,9 +8,14 @@ import { QUERY_USER } from '../graphql/queries';
 
 function DeleteDialog(props) {
   const { userId } = props;
+  const [user, setUser] = useState({});
   const [open, setOpen] = useState(false);
   const { loading, error, data } = useQuery(QUERY_USER, {
     variables: { id: Number(userId) },
+    onCompleted: () => {
+      const { user: userData } = data;
+      setUser(userData);
+    },
   });
 
   const handleOpen = () => {
@@ -19,32 +24,21 @@ function DeleteDialog(props) {
   const handleClose = () => {
     setOpen(false);
   };
-  const dialogContent = () => {
-    if (loading) {
-      return <DialogContent><CircularProgress /></DialogContent>;
-    }
 
-    if (error) {
-      return <DialogContent>{error.message}</DialogContent>;
-    }
-
-    const { user } = data;
-    return (
-      <DialogContent>
-        <Typography variant="subtitle2">Username</Typography>
-        <Typography variant="body1">{`${user.username}`}</Typography>
-        <Typography variant="subtitle2">Name</Typography>
-        <Typography variant="body1">{`${user.firstName} ${user.lastName}`}</Typography>
-      </DialogContent>
-    );
-  };
+  const { username, firstName, lastName } = user;
 
   return (
     <div>
-      <Button onClick={handleOpen}>Delete</Button>
+      <Button disabled={loading || error} onClick={handleOpen}>Delete</Button>
+      {loading && <CircularProgress />}
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Delete this user?</DialogTitle>
-        {dialogContent()}
+        <DialogContent>
+          <Typography variant="subtitle2">Username</Typography>
+          <Typography variant="body1">{`${username}`}</Typography>
+          <Typography variant="subtitle2">Name</Typography>
+          <Typography variant="body1">{`${firstName} ${lastName}`}</Typography>
+        </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Confirm</Button>
           <Button onClick={handleClose}>Cancel</Button>
--- src/Admin/EditDialog.js
+++ src/Admin/EditDialog.js
@@ -9,9 +9,33 @@ import { QUERY_USER } from '../graphql/queries';
 function EditDialog(props) {
   const { userId } = props;
   const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const { loading, error, data } = useQuery(QUERY_USER, {
     variables: { id: Number(userId) },
+    onCompleted: () => {
+      const { user } = data;
+      const {
+        username: currentUsername,
+        firstName: currentFirstName,
+        lastName: currentLastName,
+      } = user;
+      setUsername(currentUsername);
+      setFirstName(currentFirstName);
+      setLastName(currentLastName);
+    },
   });
+
+  const handleUsernameInput = (event) => {
+    setUsername(event.target.value);
+  };
+  const handleFirstNameInput = (event) => {
+    setFirstName(event.target.value);
+  };
+  const handleLastNameInput = (event) => {
+    setLastName(event.target.value);
+  };
 
   const handleOpen = () => {
     setOpen(true);
@@ -19,38 +43,40 @@ function EditDialog(props) {
   const handleClose = () => {
     setOpen(false);
   };
-  const dialogContent = () => {
-    if (loading) {
-      return <DialogContent><CircularProgress /></DialogContent>;
-    }
-
-    if (error) {
-      return <DialogContent>{error.message}</DialogContent>;
-    }
-
-    const { user } = data;
-    return (
-      <DialogContent
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <TextField label="Username" defaultValue={user.username} />
-        <TextField label="First name" defaultValue={user.firstName} />
-        <TextField label="Last name" defaultValue={user.lastName} />
-      </DialogContent>
-    );
+  const handleConfirm = () => {
+    handleClose();
   };
 
   return (
     <div>
-      <Button onClick={handleOpen}>Edit</Button>
+      <Button disabled={loading || error} onClick={handleOpen}>Edit</Button>
+      {loading && <CircularProgress />}
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Edit user</DialogTitle>
-        {dialogContent()}
+        <DialogContent
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <TextField
+            label="Username"
+            value={username}
+            onChange={handleUsernameInput}
+          />
+          <TextField
+            label="First name"
+            value={firstName}
+            onChange={handleFirstNameInput}
+          />
+          <TextField
+            label="Last name"
+            value={lastName}
+            onChange={handleLastNameInput}
+          />
+        </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Confirm</Button>
+          <Button onClick={handleConfirm}>Confirm</Button>
           <Button onClick={handleClose}>Cancel</Button>
         </DialogActions>
       </Dialog>
