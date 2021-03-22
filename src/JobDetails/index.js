f57606c009458d189ee9a0563fe9d2cd133f034e
Aaron-Goldman <aaron.goldman@moduscreate.com>
2021-03-22 19:31:37 -0400

    simplify logic

--- src/JobDetails/index.js
+++ src/JobDetails/index.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import {
   Card, CircularProgress, CardHeader, CardContent,
-  Typography, List, ListItem, ListSubheader, ListItemText,
+  Typography, List, ListItem, ListSubheader, ListItemText, Snackbar,
 } from '@material-ui/core';
 import { QUERY_JOB_DETAILS } from '../graphql/queries';
 
@@ -11,47 +11,55 @@ function JobDetails() {
   const { id } = useParams();
 
   const { loading, error, data } = useQuery(QUERY_JOB_DETAILS, {
-    variables: { id: parseInt(id, 10) },
+    variables: { id: Math.trunc(Number(id)) },
   });
-  if (loading) {
-    return (
-      <CircularProgress />
-    );
-  }
-  if (error) {
-    return (error.message);
-  }
 
-  const j = data.job;
   return (
-    <Card>
-      <CardHeader title={j.name} subheader={j.company.name} />
-      <CardContent>
-        <Typography variant="body1">
-          {j.description}
-        </Typography>
-        <List>
-          <ListSubheader>Skills</ListSubheader>
-          {j.jobSkills.map((s) => (
-            <ListItem key={s.skill.id} dense>
-              <ListItemText>{s.skill.name}</ListItemText>
-            </ListItem>
-          ))}
-          <ListSubheader>Requirements</ListSubheader>
-          {j.jobRequirements.map((r) => (
-            <ListItem key={r.id} dense>
-              <ListItemText>{r.name}</ListItemText>
-            </ListItem>
-          ))}
-          <ListSubheader>Benefits</ListSubheader>
-          {j.jobBenefits.map((b) => (
-            <ListItem key={b.id} dense>
-              <ListItemText>{b.name}</ListItemText>
-            </ListItem>
-          ))}
-        </List>
-      </CardContent>
-    </Card>
+    <div>
+      {loading && <CircularProgress />}
+      <Snackbar open={!!error}>{error && error.message}</Snackbar>
+      {data && (
+        ({
+          job: {
+            description,
+            name,
+            company: { name: companyName },
+            jobSkills,
+            jobRequirements,
+            jobBenefits,
+          },
+        }) => (
+          <Card>
+            <CardHeader title={name} subheader={companyName} />
+            <CardContent>
+              <Typography variant="body1">
+                {description}
+              </Typography>
+              <List>
+                <ListSubheader>Skills</ListSubheader>
+                {jobSkills.map(({ skill: { id: skillId, name: skillName } }) => (
+                  <ListItem key={skillId} dense>
+                    <ListItemText>{skillName}</ListItemText>
+                  </ListItem>
+                ))}
+                <ListSubheader>Requirements</ListSubheader>
+                {jobRequirements.map(({ id: requirementId, name: requirementName }) => (
+                  <ListItem key={requirementId} dense>
+                    <ListItemText>{requirementName}</ListItemText>
+                  </ListItem>
+                ))}
+                <ListSubheader>Benefits</ListSubheader>
+                {jobBenefits.map(({ id: benefitId, name: benefitName }) => (
+                  <ListItem key={benefitId} dense>
+                    <ListItemText>{benefitName}</ListItemText>
+                  </ListItem>
+                ))}
+              </List>
+            </CardContent>
+          </Card>
+        )
+      )(data)}
+    </div>
   );
 }
 
